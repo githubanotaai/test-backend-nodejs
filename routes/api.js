c497341436f4938e8df727927149a8973cccb569
andrei-majada <andreimajada@gmail.com>
2020-11-25 21:43:23 -0300

    editar e listar todos funcionando

--- routes/api.js
+++ routes/api.js
@@ -6,18 +6,6 @@ const router = express.Router();
 const Product = require('../models/product');
 
 //routers
-router.get('/api', (req, res) => {
-
-    Product.find({})
-        .then(data => {
-            console.log('Data: ', data);
-            res.json(data)
-        })
-        .catch(error => {
-            console.log('Error: ', error)
-        });
-});
-
 router.post('/create', (req, res) => {
     const NewProduct = new Product(req.body);
     console.log(req.body)
@@ -29,6 +17,36 @@ router.post('/create', (req, res) => {
             res.status('201').send('cadastro finalizado com sucesso!');
             console.log('deu certo');
         }
+    });
+})
+
+router.get('/list', (req, res) => {
+
+    Product.find({})
+        .then(data => {
+            console.log('Data: ', data);
+            res.json(data)
+        })
+        .catch(error => {
+            console.log('Error: ', error)
+        });
+});
+
+router.post('/edit/:id', (req, res) => {
+    console.log("req", req.body)
+    Product.findOne({_id:req.params.id})
+    .then(data => {
+        console.log('Data: ', data);
+        data.title = req.body.title;
+        data.description = req.body.description;
+        data.price = req.body.price;
+        data.category = req.body.category;
+        data.save();
+        res.json(data);
+    })
+    .catch(error => {
+        //console.log('Error: ', error)
+        res.json(error);
     });
 })
 
