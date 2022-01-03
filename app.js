0385f664fb94a94a069ba7f54e7e8f3c85ae5033
oyugirachel <oyugirachel20@gmail.com>
2022-01-03 20:31:44 +0300

    making changes to app.js to connect to the database

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // app.js
 
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
