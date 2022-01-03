f9090b0a251d3de679a92cbbe8644f7ffe9d9f7e
oyugirachel <oyugirachel20@gmail.com>
2022-01-03 20:12:33 +0300

    creating an entry point

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
