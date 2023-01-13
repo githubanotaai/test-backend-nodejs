81422475a816fd1a2e6641662f6a4a2953c41744
Pedro Fernandes <lebackman18@gmail.com>
2023-01-13 12:45:56 -0300

    refactor: refatoração de código

--- scripts/controllers/category-controller.js
+++ scripts/controllers/category-controller.js
@@ -1,74 +1,65 @@
 import Category from './../models/category-model.js'
+import CategoryService from './../services/category-services.js'
 
 class CategoryController {
 
     static createCategory = (require, response) => {
-        const category = new Category(require.body)
 
-        category.save(err => {
-            if (!err) {
-                response.status(201).send('Successfully create category')
-            }
+        CategoryService.createCategory(require)
+        .then(success => {
+            response.status(201).send({message:'Successfully create category'})
+        })
 
-            else {
-                response.status(500).send({ message: err.message })
-            }
+        .catch(err => {
+            response.status(500).send({ message: err.message})
         })
     }
 
     static getCategories = (require, response) => {
-        Category.find((err, categories) => {
-                if (!err) {
-                    response.status(200).send(categories)
-                }
 
-                else {
-                    response.status(400).send({ message: err.message })
-                }
-            })
+        CategoryService.getCategories()
+        .then(categories => {
+            response.status(200).send(categories)
+        })
+
+        .catch(err => {
+            response.status(400).send({ message: err.message })
+        })
     }
 
     static getCategory = (require, response) => {
-        const id = require.params.id
 
-        Category.findById(id, (err, category) => {
-            if (!err) {
-                response.status(200).send(category)
-            }
+        CategoryService.getCategory(require)
+        .then(category => {
+            response.status(200).send(category)
+        })
 
-            else {
-                response.status(400).send({ message: err.message })
-            }
+        .catch(err => {
+            response.status(400).send({ message: err.message })
         })
 
     }
 
     static updateCategory = (require, response) => {
-        const id = require.params.id
 
-        Category.findByIdAndUpdate(id, { $set: require.body }, err => {
-            if (!err) {
-                response.status(200).send({ message: 'Successfully update category' })
-            }
+        CategoryService.updateCategory(require)
+        .then(success => {
+            response.status(200).send({ message: "Successfully delete category" })
+        })
 
-            else {
-                response.status(500).send({ message: err.message })
-            }
+        .catch(err => {
+            response.status(500).send({message: err.message})
         })
     }
 
     static deleteCategory = (require, response) => {
-        const id = require.params.id
+        CategoryService.deleteCategory(require)
+        .then(success => {
+            response.status(200).send({message: "Successfully delete category"})
+        })
 
-        Category.findByIdAndDelete(id, err => {
-
-            if (!err) {
-                response.status(200).send({ message: 'Successfully delete category' })
-            }
-
-            else {
-                response.status(500).send({ messagea: err.message })
-            }
+        .catch(err => {
+            response.status(500).send({message: err.message})
         })
     }
 }
